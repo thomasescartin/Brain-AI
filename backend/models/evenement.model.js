0dcda95f3c1634db5f6a8ec3df8f858c7fc44825
thomasescartin <thomas.escartin@gmail.com>
2026-05-21 14:13:32 +0200

    Configuration du model des projets plus modification sur le model évènements.

--- backend/models/evenement.model.js
+++ backend/models/evenement.model.js
@@ -1,5 +1,6 @@
 import { db } from "../config/db.js";
 
+//trouver un évènement
 export const trouverEvenement = async (id_evenement) => {
   const [sql] = await db.query(
     "SELECT * FROM evenements WHERE id_evenement = ?",
@@ -8,6 +9,7 @@ export const trouverEvenement = async (id_evenement) => {
   return sql[0];
 };
 
+// Créer un évènement
 export const creerEvenement = async (titre, date_evenement, contenue) => {
   const [sql] = await db.query(
     "INSERT INTO evenements (titre, date_evenement, contenue) VALUES (?,?,?)",
@@ -16,6 +18,7 @@ export const creerEvenement = async (titre, date_evenement, contenue) => {
   return sql.insertId;
 };
 
+//Modifier un évènement
 export const modifEvenement = async (titre, date_evenement, contenue) => {
   const [sql] = await db.query(
     "UPDATE evenements SET titre = ?, date_evenement = ?, contenue = ?",
@@ -24,6 +27,7 @@ export const modifEvenement = async (titre, date_evenement, contenue) => {
   return sql.affectedRows;
 };
 
+//Supprimmer un évènement
 export const supprimmerEvenement = async (id_evenement) => {
   const [sql] = await db.query("DELETE FROM evenement WHERE id_evenement = ?", [
     id_evenement,
---
+++ backend/models/projet.model.js
@@ -0,0 +1,34 @@
+import { db } from "../config/db.js";
+
+export const trouverProjet = async (id_projet) => {
+  const [sql] = await db.query("SELECT * FROM projets WHERE id_projet = ?", [
+    id_projet,
+  ]);
+  return sql[0];
+};
+
+//Créer un projet
+export const creerProjet = async (titre, date_projet) => {
+  const [sql] = await db.query(
+    "INSERT INTO projets (titre, date_projet) VALUES (?, ?)",
+    [titre, date_projet]
+  );
+  return sql.insertId;
+};
+
+//Modifier un projet
+export const modifierProjet = async (id_projet, titre, date_projet) => {
+  const [sql] = await db.query(
+    "UPDATE projets SET titre = ?, date_projet = ? WHERE id_projet= ?",
+    [id_projet, titre, date_projet]
+  );
+  return sql.affectedRows;
+};
+
+//Suuprimmer un projet
+export const supprimmerProjet = async (id_projet) => {
+  const [sql] = await db.query("DELETE FROM projets WHERE id_projet = ?", [
+    id_projet,
+  ]);
+  return sql.affectedRows;
+};
