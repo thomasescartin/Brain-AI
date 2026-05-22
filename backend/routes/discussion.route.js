3e69bf6da22bcde57f678e1d082483486b412252
thomasescartin <thomas.escartin@gmail.com>
2026-05-22 10:42:21 +0200

    Configuration des routes des discussions.

---
+++ backend/routes/discussion.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  creationDiscussion,
+  afficherDiscussion,
+  modifDiscussion,
+  supprDiscussion,
+} from "../controllers/discussion.controller";
+
+const discussionRoute = express.Router();
+
+//Routes d'affichage d'une discussion
+discussionRoute.post("/:id_utilisateur", creationDiscussion);
+discussionRoute.post("/:id_discussion", afficherDiscussion);
+
+//Route de gestion des discussion
+discussionRoute.put("/:id_discussion", modifDiscussion);
+discussionRoute.delete("/:id_discission", supprDiscussion);
+
+export default discussionRoute;
