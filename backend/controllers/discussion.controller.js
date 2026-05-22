ca1b140b8762dec71d96bfa68a39d9d0726d61cd
thomasescartin <thomas.escartin@gmail.com>
2026-05-22 10:34:00 +0200

    Configuration du controlleur de discussions.

---
+++ backend/controllers/discussion.controller.js
@@ -0,0 +1,84 @@
+import * as discussion from "../models/discussion.model.js";
+
+//Création de la discussion
+export const creationDiscussion = async (req, res) => {
+  try {
+    const { id_utilisateur } = req.params;
+    const { titre, contenue } = req.body;
+    //Validation de la discussion
+    if (!titre || !contenue) {
+      return res
+        .status(400)
+        .json({ message: "Tous les champs sont obligatoires." });
+    }
+    //Vérification si la discussion existe déjà
+    const discussionExistante = await discussion.trouverDiscussion(titre);
+    if (discussionExistante) {
+      return res.status(409).json({ message: "Discussion déjà existante." });
+    }
+    await discussion.creerDiscussion(titre, contenue, id_utilisateur);
+    res.status(201).json({ message: "Discussion créée." });
+  } catch (error) {
+    console.error("Erreur d'enregistrement:", error.message);
+    res.status(500).json({ message: "Erreur serveur." });
+  }
+};
+
+// Affichage de la discussion
+export const afficherDiscussion = async (req, res) => {
+  try {
+    const { id_discussion } = req.params;
+    //Vérifier si la discussion existe déjà
+    const discussionExistante = await discussion.trouverDiscussion(
+      id_discussion
+    );
+    if (!discussionExistante) {
+      return res.status(404).json({ message: "Discussion inexistante." });
+    }
+    //Retrouver la discussion
+    res.status(200).json(discussionExistante);
+  } catch (error) {
+    console.error("Erreur d'affichage.", error.message);
+    res.status(500).json({ message: "Erreur serveur." });
+  }
+};
+
+//Modifier la discussion
+export const modifDiscussion = async (req, res) => {
+  try {
+    const { id_discussion } = req.params;
+    const modification = await discussion.modifierDiscussion(
+      id_discussion,
+      req.body.titre,
+      req.body.contenue
+    );
+    if (!modification) {
+      return res.status(400).json({ message: "Discussion non modifiée." });
+    }
+    res.json({ message: "Discussion modifiée." });
+  } catch (error) {
+    console.error("Erreur modification :", error.message);
+
+    res.status(500).json({
+      message: "Erreur serveur.",
+    });
+  }
+};
+
+//Supprimer la discussion
+export const supprDiscussion = async (req, res) => {
+  try {
+    const { id_discussion } = req.params;
+    const supprimer = await discussion.supprimerDiscussion(id_discussion);
+    if (!supprimer) {
+      res.status(400).json({ message: "Discussion non trouvée." });
+    }
+    res.json({ message: "Discussion suprimée." });
+  } catch (error) {
+    console.error("Erreur suppression :", error.message);
+
+    res.status(500).json({
+      message: "Erreur serveur.",
+    });
+  }
+};
