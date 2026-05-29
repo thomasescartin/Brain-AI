652eaa13bbccd8a1f86092da6118c73b7123a67d
thomasescartin <thomas.escartin@gmail.com>
2026-05-29 09:21:24 +0200

    Gestion d'erreur et changement dans la page d'inscription, rajout des label et imputs Prenom et Nom.

--- client/pages/inscription.jsx
+++ client/pages/inscription.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-// import "./Formulaires.css";
+import { Link, useNavigate } from "react-router-dom";
+import "../style/formulaire.css";
 
 export default function Inscription() {
+  const [prenom, setPrenom] = useState("");
+  const [nom, setNom] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setError("");
 
     if (password !== confirmPassword) {
       setError("Les mots de passe ne correspondent pas");
@@ -20,79 +24,127 @@ export default function Inscription() {
 
     try {
       const response = await fetch(
-        "http://sql7.freesqldatabase.com:3306/api/auth",
+        "http://localhost:5000/api/utilisateurs/register",
         {
           method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          body: JSON.stringify({
+            prenom,
+            nom,
+            email,
+            mot_de_passe: password,
+          }),
         }
       );
 
       const data = await response.json();
 
       if (!response.ok) {
-        throw new Error(data.error);
+        throw new Error(data.message);
       }
-
+      setPrenom("");
+      setNom("");
       setEmail("");
       setPassword("");
       setConfirmPassword("");
 
-      navigate("/dashboard"); // redirection vers le dashboard
+      navigate("/profile");
     } catch (err) {
       setError(err.message);
     }
   };
 
   function securiteMDP(password) {
-    if (/[<>`"'\\;]/.test(password)) return false; // invalide si un caractère spéciale est dans le mot de passe
+    if (/[<>`"'\\;]/.test(password)) return false;
+
     return (
-      password.length >= 8 && // le mot de passe doit être supèrieur ou égal à 8 caractères
-      /[A-Z]/.test(password) && // test si il y a au moins une majuscule
-      /[a-z]/.test(password) && // test si il y a ua moins une minuscules
-      /\d/.test(password) // d = digit , permet de tester si il y a au moins un chiffre entre 0 et 9 inclus
+      password.length >= 8 &&
+      /[A-Z]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /\d/.test(password)
     );
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h2>Inscription </h2>
-        </div>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" name="" id="" placeholder="Email" required />
-        </div>
-        <div>
-          <label htmlFor="">Mot de passe</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Mot de passe"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="">Validation mot de passe</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Mot de passe"
-            required
-          />
-        </div>
+    <form onSubmit={handleSubmit}>
+      <h2>Inscription</h2>
 
+      <div>
+        <label>Prénom</label>
+
+        <input
+          type="text"
+          value={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+          placeholder="Prénom"
+        />
+      </div>
+      <div>
+        <label>Nom</label>
+
+        <input
+          type="text"
+          value={nom}
+          onChange={(e) => setNom(e.target.value)}
+          placeholder="Nom"
+        />
+      </div>
+
+      <div>
+        <label>Email</label>
+
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+      </div>
+
+      <div>
+        <label>Mot de passe</label>
+
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Mot de passe"
+          required
+        />
+      </div>
+
+      <div>
+        <label>Confirmation mot de passe</label>
+
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder="Confirmation"
+          required
+        />
+      </div>
+
+      {!securiteMDP(password) && (
         <p>
-          Vous avez déjà un compte ? <Link to="/login">Se connecter</Link>
+          Le mot de passe doit contenir : 8 caractères, une majuscule, une
+          minuscule et un chiffre.
         </p>
+      )}
 
-        {error && <p>{error}</p>}
+      {error && <p>{error}</p>}
 
-        <button disabled={!securiteMDP(password)}>S'inscrire</button>
-      </form>
-    </>
+      <button type="submit" disabled={!securiteMDP(password)}>
+        S'inscrire
+      </button>
+
+      <p>
+        Vous avez déjà un compte ?<Link to="/login"> Se connecter</Link>
+      </p>
+    </form>
   );
 }
