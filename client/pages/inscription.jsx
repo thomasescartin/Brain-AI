e506989eb449cc10a9ad7fe2b7ddf436df522943
thomasescartin <thomas.escartin@gmail.com>
2026-06-01 22:07:23 +0200

    Ajout du style dans la page d'inscription avec les média queries.

--- client/pages/inscription.jsx
+++ client/pages/inscription.jsx
@@ -70,81 +70,76 @@ export default function Inscription() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Inscription</h2>
+      <div className="formulaire">
+        {" "}
+        <h2>Inscription</h2>
+        <div>
+          <label>Prénom</label>
 
-      <div>
-        <label>Prénom</label>
+          <input
+            type="text"
+            value={prenom}
+            onChange={(e) => setPrenom(e.target.value)}
+            placeholder="Prénom"
+          />
+        </div>
+        <div>
+          <label>Nom</label>
 
-        <input
-          type="text"
-          value={prenom}
-          onChange={(e) => setPrenom(e.target.value)}
-          placeholder="Prénom"
-        />
-      </div>
-      <div>
-        <label>Nom</label>
+          <input
+            type="text"
+            value={nom}
+            onChange={(e) => setNom(e.target.value)}
+            placeholder="Nom"
+          />
+        </div>
+        <div>
+          <label>Email</label>
 
-        <input
-          type="text"
-          value={nom}
-          onChange={(e) => setNom(e.target.value)}
-          placeholder="Nom"
-        />
-      </div>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div>
+          <label>Mot de passe</label>
 
-      <div>
-        <label>Email</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Mot de passe"
+            required
+          />
+        </div>
+        <div>
+          <label>Confirmation mot de passe</label>
 
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-      </div>
-
-      <div>
-        <label>Mot de passe</label>
-
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Mot de passe"
-          required
-        />
-      </div>
-
-      <div>
-        <label>Confirmation mot de passe</label>
-
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder="Confirmation"
-          required
-        />
-      </div>
-
-      {!securiteMDP(password) && (
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirmation"
+            required
+          />
+        </div>
+        {!securiteMDP(password) && (
+          <p>
+            Le mot de passe doit contenir : 8 caractères, une majuscule, une
+            minuscule et un chiffre.
+          </p>
+        )}
+        {error && <p>{error}</p>}
+        <button type="submit" disabled={!securiteMDP(password)}>
+          S'inscrire
+        </button>
         <p>
-          Le mot de passe doit contenir : 8 caractères, une majuscule, une
-          minuscule et un chiffre.
+          Vous avez déjà un compte ?<Link to="/login"> Se connecter</Link>
         </p>
-      )}
-
-      {error && <p>{error}</p>}
-
-      <button type="submit" disabled={!securiteMDP(password)}>
-        S'inscrire
-      </button>
-
-      <p>
-        Vous avez déjà un compte ?<Link to="/login"> Se connecter</Link>
-      </p>
+      </div>
     </form>
   );
 }
