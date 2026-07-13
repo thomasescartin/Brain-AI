f02dda5bf68109c635b7c8a2a873e975dd442ed5
thomasescartin <thomas.escartin@gmail.com>
2026-07-13 15:56:39 +0200

    importation du style global dans le main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import "../style/global.css";
+
 import App from "./app.jsx";
 
 createRoot(document.getElementById("root")).render(
