9873baac6267a5f8516a0f20b6fa817e9de5ebec
thomasescartin <thomas.escartin@gmail.com>
2026-07-13 16:21:57 +0200

    Création du composant avatar et son style.

---
+++ client/components/Avatar.jsx
@@ -0,0 +1,25 @@
+import "../style/Avatar.css";
+
+import avatarDefault from "../../assets/avatar.png";
+
+export default function Avatar({
+  src,
+
+  prenom = "",
+
+  nom = "",
+
+  size = "medium",
+}) {
+  const initials = `${prenom.charAt(0)}${nom.charAt(0)}`.toUpperCase();
+
+  return (
+    <div className={`avatar ${size}`}>
+      {src ? (
+        <img src={src} alt={`${prenom} ${nom}`} />
+      ) : (
+        <span>{initials || "?"}</span>
+      )}
+    </div>
+  );
+}
