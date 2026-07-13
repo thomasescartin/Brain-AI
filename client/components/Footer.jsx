a4f97a0b0ae6c728ee7652a61a27db0fccf7fba5
thomasescartin <thomas.escartin@gmail.com>
2026-07-13 14:44:42 +0200

    Ajout des composants Header et Footer ainsi que leur style.

---
+++ client/components/Footer.jsx
@@ -0,0 +1,7 @@
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">© 2026 Brain_AI — Tous droits réservés</footer>
+  );
+}
---
+++ client/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/logo.png" alt="Brain_AI" />
+
+        <h1>Brain_AI</h1>
+      </div>
+
+      <nav>
+        <Link to="/">Accueil</Link>
+
+        <Link to="/discussion">Discussions</Link>
+
+        <Link to="/projets">Projets</Link>
+
+        <Link to="/evenements">Evènements</Link>
+
+        <Link to="/profile">Profil</Link>
+      </nav>
+    </header>
+  );
+}
