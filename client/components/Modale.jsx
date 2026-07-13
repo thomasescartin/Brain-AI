70cb312d7bc009c0c93662b43270424cd99de6a1
thomasescartin <thomas.escartin@gmail.com>
2026-07-13 16:12:05 +0200

    Création du composant modale et de son style.

---
+++ client/components/Modale.jsx
@@ -0,0 +1,21 @@
+import "../style/Modal.css";
+
+export default function Modal({ isOpen, onClose, title, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <h2>{title}</h2>
+
+          <button className="modal-close" onClick={onClose}>
+            ✖
+          </button>
+        </div>
+
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>
+  );
+}
