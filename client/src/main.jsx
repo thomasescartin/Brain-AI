cf4b0a7e94edf8a83c92d73406447f4c8c6025df
thomasescartin <thomas.escartin@gmail.com>
2026-07-14 17:31:39 +0200

    Changement du main.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext.jsx";
 
 import "../style/global.css";
 
@@ -9,7 +10,9 @@ import App from "./app.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
