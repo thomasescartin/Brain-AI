31f37c76ed7a82111bec368b55e7f73f5177cae1
thomasescartin <thomas.escartin@gmail.com>
2026-07-14 17:20:35 +0200

    Ajout du service API.

---
+++ client/service/api.js
@@ -0,0 +1,27 @@
+const API_URL = "http://localhost:5000/api";
+
+export async function apiFetch(endpoint, options = {}) {
+  const token = localStorage.getItem("token");
+
+  const headers = {
+    "Content-Type": "application/json",
+    ...options.headers,
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const response = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers,
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Erreur serveur");
+  }
+
+  return data;
+}
