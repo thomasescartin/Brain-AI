9c0971ed508eb840f93de71e37d401503d55a504
thomasescartin <thomas.escartin@gmail.com>
2026-08-04 15:13:14 +0200

    Modification du modele des évènements.

--- backend/models/evenement.model.js
+++ backend/models/evenement.model.js
@@ -1,36 +1,93 @@
 import { db } from "../config/db.js";
 
-//trouver un évènement
-export const trouverEvenement = async (id_evenement) => {
-  const [sql] = await db.query(
+// Rechercher un évènement par son ID
+export const trouverEvenementParId = async (id_evenement) => {
+  const [rows] = await db.query(
     "SELECT * FROM evenements WHERE id_evenement = ?",
     [id_evenement]
   );
-  return sql[0];
+
+  return rows[0];
+};
+
+// Rechercher un évènement par son titre
+export const trouverEvenementParTitre = async (titre) => {
+  const [rows] = await db.query("SELECT * FROM evenements WHERE titre = ?", [
+    titre,
+  ]);
+
+  return rows[0];
 };
 
 // Créer un évènement
-export const creerEvenement = async (titre, date_evenement, contenue) => {
-  const [sql] = await db.query(
-    "INSERT INTO evenements (titre, date_evenement, contenue) VALUES (?,?,?)",
-    [titre, date_evenement, contenue]
+export const creerEvenement = async (
+  titre,
+  description,
+  lieu,
+  date_evenement,
+  id_utilisateur
+) => {
+  const [result] = await db.query(
+    `INSERT INTO evenements
+    (
+      titre,
+      description,
+      lieu,
+      date_evenement,
+      id_utilisateur
+    )
+    VALUES (?, ?, ?, ?, ?)`,
+    [titre, description, lieu, date_evenement, id_utilisateur]
   );
-  return sql.insertId;
+
+  return result.insertId;
 };
 
-//Modifier un évènement
-export const modifEvenement = async (titre, date_evenement, contenue) => {
-  const [sql] = await db.query(
-    "UPDATE evenements SET titre = ?, date_evenement = ?, contenue = ?",
-    [titre, date_evenement, contenue]
+// Modifier un évènement
+export const modifierEvenement = async (
+  id_evenement,
+  titre,
+  description,
+  lieu,
+  date_evenement
+) => {
+  const [result] = await db.query(
+    `UPDATE evenements
+     SET
+        titre = ?,
+        description = ?,
+        lieu = ?,
+        date_evenement = ?
+     WHERE id_evenement = ?`,
+    [titre, description, lieu, date_evenement, id_evenement]
   );
-  return sql.affectedRows;
+
+  return result.affectedRows;
 };
 
-//Supprimmer un évènement
-export const supprimmerEvenement = async (id_evenement) => {
-  const [sql] = await db.query("DELETE FROM evenement WHERE id_evenement = ?", [
-    id_evenement,
-  ]);
-  return sql.affectedRows;
+// Supprimer un évènement
+export const supprimerEvenement = async (id_evenement) => {
+  const [result] = await db.query(
+    "DELETE FROM evenements WHERE id_evenement = ?",
+    [id_evenement]
+  );
+
+  return result.affectedRows;
+};
+
+// Tous les évènements
+export const tousLesEvenements = async () => {
+  const [rows] = await db.query(`
+    SELECT
+      e.*,
+      u.prenom,
+      u.nom,
+      u.photo_utilisateur
+    FROM evenements e
+    JOIN utilisateurs u
+      ON e.id_utilisateur = u.id_utilisateur
+    ORDER BY e.date_evenement ASC
+  `);
+
+  return rows;
 };
