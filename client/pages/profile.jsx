3e17c7f6f99eaef2e9982052a1185ee89d4c75e5
thomasescartin <thomas.escartin@gmail.com>
2026-08-10 19:14:04 +0200

    Modification de la page de profile.

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import "../style/formulaire.css";
+import "../style/profileStyle.css";
 
 export default function Profil() {
   const navigate = useNavigate();
