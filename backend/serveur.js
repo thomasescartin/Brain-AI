5f0190d10a09bda20ce7ac1c6ab430721914c21e
thomasescartin <thomas.escartin@gmail.com>
2026-08-10 18:45:31 +0200

    Ajout de upload dans le serveur.

--- backend/serveur.js
+++ backend/serveur.js
@@ -20,6 +20,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 app.use(cors("http://localhost:5173"));
 
 app.use("/api/utilisateurs", utilisateurRoute);
